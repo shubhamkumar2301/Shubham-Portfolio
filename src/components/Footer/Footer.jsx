b508a6c07d5c18a2652ece412295dd80ac2f0edf
shubham <shubham2345567@gmail.com>
2025-04-22 23:22:00 +0530

    error Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './footer.css'
+import './Footer.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import user_icon from '../../assets/user_icon.svg'
 
